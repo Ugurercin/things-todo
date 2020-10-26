f2d02016eea1bf6b401adcc5cae3d9f5a7cfdeae
ugur <ercinugur@gmail.com>
2020-10-26 14:45:52 +0300

    todo class initialized

--- script/app.js
+++ script/app.js
@@ -6,3 +6,22 @@ document.addEventListener('DOMContentLoaded', e => {
     console.error("done")
   }, 1);
 })
+
+const todoUserInput = document.getElementById("todo-input");
+const todoUserInputButton = document.getElementById('todo-input-button');
+
+
+class TodoItem {
+  constructor(image, todo, priority, initilDate, dueDate, state) {
+    this.image = image;
+    this.todo = todo;
+    this.priority = priority;
+    this.initilDate = initilDate;
+    this.dueDate = dueDate;
+    this.state = state;
+  }
+  
+}
+
+
+
